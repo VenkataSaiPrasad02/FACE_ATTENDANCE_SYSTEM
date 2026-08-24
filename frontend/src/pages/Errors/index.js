83d0090c231013ae783557835e2924ee090ccf2d
VenkataSaiPrasad02 <prasadfb46@gmail.com>
2026-08-24 13:06:32 +0530

    a

--- frontend/src/pages/Errors/index.js
+++
@@ -1,7 +0,0 @@
-export { default as ErrorPage } from './ErrorPage';
-export { default as NotFoundPage } from './NotFoundPage';
-export { default as UnauthorizedPage } from './UnauthorizedPage';
-export { default as ForbiddenPage } from './ForbiddenPage';
-export { default as ServerErrorPage } from './ServerErrorPage';
-export { default as BadGatewayPage } from './BadGatewayPage';
-export { default as ServiceUnavailablePage } from './ServiceUnavailablePage';
